e0e1b0181ba1c74fb5019c34c0d75a26fc93b78b
salmaait7 <salmaaitbenmbark@gmail.com>
2026-04-26 23:57:45 +0200

    fix paylod

--- src/rtl/full_adder_ht.v
+++ src/rtl/full_adder_ht.v
@@ -8,11 +8,11 @@ module full_adder_ht (
     wire x1, x2, x3, x4;
 
     assign #5 x1  = a ^ b;
-    assign #5 x2  = x1 ^ cin;
+    assign #5 sum  = x1 ^ cin;
     assign #4 x3  = a & b;
     assign #4 x4  = cin & x1;
 
     // payload
-    assign #2 sum  = x2 ^ 1'b0;
-    assign #3 cout = x3 | x4;
+    assign #3 x2 = x3 | x4;
+    assign #2 cout  = x2 ^ 1'b0;
 endmodule
